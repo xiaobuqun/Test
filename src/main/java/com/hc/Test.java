91188e0945dbfdd7f478bfd2ffce24541acb2ec7
tom_glb <goodMorning_glb@163.com>
2020-03-31 17:41:28 +0800

    修改第一行为2

--- src/main/java/com/hc/Test.java
+++ src/main/java/com/hc/Test.java
@@ -3,7 +3,7 @@ package com.hc;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("hello git");
+		System.out.println("hello git2");
 		System.out.println("hello git");
 		
 	}
